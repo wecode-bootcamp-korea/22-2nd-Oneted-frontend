4ab0129a49ddf01c7be1dcc767206f775c0b556b
choiuhana <choiuhana@gmail.com>
2021-07-22 19:11:55 +0900

    ADD: 라이브러리 활용한 메인 페이지 슬라이드 구현 ADD: main 슬라이드 라이브러리 기능 추가 Modify: 메인페이지 충돌부분 수정 슬라이드 사진 수정

---
+++ src/pages/Main/MainSlider/MainSlider.js
@@ -0,0 +1,54 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, A11y, Autoplay } from 'swiper';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import styled from 'styled-components';
+
+SwiperCore.use([Navigation, Autoplay, A11y]);
+
+function MainSlider() {
+  return (
+    <>
+      <Swiper navigation {...settings}>
+        {SLIDE_IMAGE.map(image => (
+          <SwiperSlide>
+            <Slide url={image.url} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
+
+export default MainSlider;
+
+const Slide = styled.div`
+  background-image: url(${props => props.url});
+  background-position: bottom;
+  background-size: cover;
+  height: 400px;
+  margin-left: auto;
+  margin-top: 51px;
+  right: 0;
+`;
+
+const settings = {
+  slidesPerView: 'auto',
+  loop: true,
+  loopAdditionalSlides: 1,
+  autoplay: {
+    delay: 5000,
+  },
+};
+
+const SLIDE_IMAGE = [
+  {
+    url: 'images/slide1.jpg',
+  },
+  {
+    url: 'images/slide2.jpg',
+  },
+  {
+    url: 'images/slide3.jpg',
+  },
+];
